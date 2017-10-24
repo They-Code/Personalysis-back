2e31c2c930d486aa3ffd1907a5628fbd042a56b4
Azhar Deen <ahmdeen@gmail.com>
2017-10-23 21:09:52 -0700

    second commit

--- routes/api/awakens/awakens.js
+++ routes/api/awakens/awakens.js
@@ -2,33 +2,6 @@ const router = require("express").Router();
 const axios = require("axios");
 
 
-
-
-router.route('/')
-    .all(function (req, res, next) {
-
-        next();
-    })
-    .get(function (req, res, next) {
-        console.log(req);
-        axios.get('https://api.yelp.com/v3/businesses/search', {
-            params: {
-                term: req.query.term,
-                location: req.query.location
-            },
-            headers: {
-                Authorization: "Bearer GENOMELINKTEST"
-            }
-        }
-        )
-            .then(function (response) {
-                res.json(response.data);
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    });
-
 router.route('/:phenotype')
     .all(function (req, res, next) {
         // runs for all HTTP verbs first
@@ -55,3 +28,29 @@ router.route('/:phenotype')
 
 
 module.exports = router;
+
+
+// router.route('/')
+//     .all(function (req, res, next) {
+
+//         next();
+//     })
+//     .get(function (req, res, next) {
+//         console.log(req);
+//         axios.get('https://api.yelp.com/v3/businesses/search', {
+//             params: {
+//                 term: req.query.term,
+//                 location: req.query.location
+//             },
+//             headers: {
+//                 Authorization: "Bearer GENOMELINKTEST"
+//             }
+//         }
+//         )
+//             .then(function (response) {
+//                 res.json(response.data);
+//             })
+//             .catch(function (error) {
+//                 console.log(error);
+//             });
+//     });
